8ae2608be643bc2fd11c2d9d6651c952b60cae29
Iqbal <hosiqb@gmail.com>
2020-06-23 20:14:09 +0600

    my changes3

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -37,9 +37,9 @@ class AuthServiceProvider extends ServiceProvider
            return $user->role == 3;
         });
 
-        Gate::define('update-post', function ($user, $post) {
-           return $user->id == $post->user_id;
-        });
+        // Gate::define('update-post', function ($user, $post) {
+        //    return $user->id == $post->user_id;
+        // });
 
     }
 }
